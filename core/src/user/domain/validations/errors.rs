76cc4b2c13632a01a79f28915fed572363d7c647
JtMe <johnnatanmartinezerazo@gmail.com>
2025-10-07 14:33:00 -0500

    separación de errores y regex

---
+++ core/src/user/domain/validations/errors.rs
@@ -0,0 +1,58 @@
+use std::fmt;
+
+use crate::user::domain::vo::UserStatus;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum CategoryError {
+    Id,
+    ExternalId,
+    Username,
+    Email,
+    Phone,
+    Status,
+    Role,
+    Locale,
+    Timezone,
+    Gender,
+    AuthType,
+    SubscriptionTier,
+    SubscriptionStatus,
+    ConsentType,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum TypeError {
+    Empty,
+    Missing,
+    NotSupported,
+    Format { format: String,},
+    TooShort { short: i16,},
+    TooLong { long: i32,},
+    Unchanged { value: String,},
+    Characters { value: String,},
+    StartsWithChar { start: String,},
+    EndsWithChar { end: String,},
+    AlreadyVerified { value: String,},
+    InvalidStatus { status: UserStatus,},
+    Transition { from: UserStatus, to: UserStatus, },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct  UserDomainError {
+    category: CategoryError,
+    detail: TypeError,
+}
+
+impl From<(CategoryError, TypeError)> for UserDomainError {
+    fn from((category, detail): (CategoryError, TypeError)) -> Self {
+        Self { category, detail }
+    }
+}
+
+impl fmt::Display for UserDomainError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error {:?} -> {:?}", self.category, self.detail)
+    }
+}
+
+impl std::error::Error for UserDomainError {}
---
+++ core/src/user/domain/validations/mod.rs
@@ -0,0 +1,5 @@
+pub mod errors;
+pub mod regexes;
+
+pub use errors::*;
+pub use regexes::*;
