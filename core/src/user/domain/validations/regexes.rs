8d49283d0c1ff2be03ca35526adc1ef0e6acab81
JtMe <johnnatanmartinezerazo@gmail.com>
2025-10-17 20:20:51 -0500

    refactor regex initialization to use expect for error handling

--- core/src/user/domain/validations/regexes.rs
+++ core/src/user/domain/validations/regexes.rs
@@ -10,15 +10,15 @@ pub static EMAIL_REGEX: LazyLock<ValidationRule> = LazyLock::new(|| ValidationRu
     name: "EMAIL_REGEX",
     regex: Regex::new(
         r"^[A-Za-z0-9](?:[A-Za-z0-9._-]{2,63}[A-Za-z0-9])?@[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?(?:\.[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*\.[A-Za-z]{2,24}$"
-    ).unwrap(),
+    ).expect("Invalid EMAIL_REGEX"),
 });
 
 pub static ROLE_NAME_REGEX: LazyLock<ValidationRule> = LazyLock::new(|| ValidationRule {
     name: "ROLE_NAME_REGEX",
-    regex: Regex::new(r"^[a-z][a-z0-9_-]{2,49}$").unwrap()
+    regex: Regex::new(r"^[a-z][a-z0-9_-]{2,49}$").expect("Invalid ROLE_NAME_REGEX"),
 });
 
 pub static USERNAME_REGEX: LazyLock<ValidationRule> = LazyLock::new(|| ValidationRule {
     name: "USERNAME_REGEX",
-    regex: Regex::new(r"^(?=.{6,30}$)[a-z][a-z0-9](?:[._-]?[a-z0-9])*$").unwrap()
+    regex: Regex::new(r"^(?=.{6,30}$)[a-z][a-z0-9](?:[._-]?[a-z0-9])*$").expect("Invalid USERNAME_REGEX"),
 });
